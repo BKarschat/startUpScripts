5d56589f2eb3c690c27a435e5817a041d826eb18
Bastian Karschat <Bastian.Karschat@gmail.com>
2020-12-07 14:58:38 +0100

    Update ssh_commands.sh

--- ssh_commands.sh
+++ ssh_commands.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-
-ssh-keygen -t ed25519 -C "bastian.karschat@gmail.com"
-cat ~/.ssh/todo
+sudo apt update
+sudo apt upgrade
+sudo apt install virtualbox-ext-pack
+sudo apt install virtualbox-guest-dkms
+sudo apt install vim
+sudo apt install htop
+sudo apt install zsh
+sudo apt install tmux
+mkdir tmux
+cd tmux
+sudo apt install git
+git clone https://github.com/gpakosz/.tmux.git
+chsh -s $(`which zsh`) $USER
+echo "set-option -g default-shell /bin/zsh" >> .tmux.conf.local
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
+# vim .zshrc -> change plugin to "agnoster"# tmux new -s SESSIONNAME
+# TODO ADD 
+# ssh-keygen -t ed25519 -C "user@aconno.com"
+reboot
